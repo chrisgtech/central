253fbd9e5ffbaff0daec48de8daa670d368aa7d3
Cheryl Miller <cmiller112000@sbcglobal.net>
2015-03-21 19:24:27 -0500

    restructure pictures just by gender.  to few pictures with too many older patients

--- addpatientpicture.py
+++ addpatientpicture.py
@@ -12,32 +12,14 @@ CACHE_FILE = 'patients.json'
 
 
 def encodeImages():
-    encodedImages = { 'man': {}, 'woman': {}}
+    encodedImages = { 'man': [], 'woman': []}
     genders = {"men": "man","women":"woman"}
-    ageranges = ["age20-39","age40-59","age60+"]
-
-    with open('central_site/static/img/no_photo.jpg','rb') as img:
-        encoded_str = base64.b64encode(img.read())
-        encodedImages['man']['any'] = []
-        encodedImages['man']['any'].append("data:image/jpg;base64," + encoded_str)
-
-    with open('central_site/static/img/woman_nophoto_big.jpg','rb') as img:
-        encoded_str = base64.b64encode(img.read())
-        encodedImages['woman']['any'] = []
-        encodedImages['woman']['any'].append("data:image/jpg;base64," + encoded_str)
-
 
     for g,f in genders.iteritems():
-        for i in xrange(len(ageranges)):
-            encodedImages[f][ageranges[i]] = []
-            for fnum in xrange(4):
-                fname = 'central_site/static/img/' + g + '/' + ageranges[i] + '/' + f + str(fnum + 1) + '.jpg'
-                img = open(fname,'rb')
-                if img:
-                    idata = img.read()
-                    encodedImages[f][ageranges[i]].append("data:image/jpg;base64," + base64.b64encode(idata))
-                    img.close()
-                    del idata
+        for fnum in xrange(12):
+            fname = 'central_site/static/img/' + g + '/' + f + str(fnum + 1) + '.jpg'
+            with open(fname,'rb') as img:
+                encodedImages[f].append("data:image/jpg;base64," + base64.b64encode(img.read()))
                     
     return encodedImages
 
@@ -46,25 +28,6 @@ def getGender(ingender):
     if g == 'f' or g == 'female':
         return "woman"
     return "man"
-    
-def getAgeRange(dob):
-    agerange = "any"
-    if len(dob) > 0:
-        dateparts = dob.split('-')
-        perdob = datetime.date(int(dateparts[0]),int(dateparts[1]),int(dateparts[2]))
-        thisday = datetime.date.today()
-        ageinyears = thisday.year - perdob.year
-        if thisday.month < perdob.month:
-            ageinyears += 1
-        elif thisday.day <= perdob.day:
-            ageinyears += 1
-        if ageinyears >= 20 and ageinyears <= 39:
-            agerange = "age20-39"
-        elif ageinyears >= 40 and ageinyears <= 59:
-            agerange = "age40-59"
-        elif ageinyears >= 60:
-            agerange = "age60+"
-    return agerange
     
     
 def get_json(url):
@@ -77,14 +40,10 @@ def put_json_tocache(data):
         json.dump(data, cachefile)
     
 
-def getPicture(images, gender,dob):
+def getPicture(images, gender):
     g = getGender(gender)
-    a = getAgeRange(dob)
-    p = 0
-    if a != 'any':
-        p = random.randint(0,3)
-    
-    return images[g][a][p]
+    p = random.randint(0,11)
+    return images[g][p]
 
 def main(argv=None):
     images = encodeImages()
@@ -99,15 +58,11 @@ def main(argv=None):
         
     for i in xrange(numentries):
         if entries[i].has_key('content'):
-            if (entries[i]['content'].has_key('birthDate')):
-                bdate = entries[i]['content']['birthDate']
-            else:
-                bdate = ""
             if (entries[i]['content'].has_key('gender')):
                 gender = entries[i]['content']['gender']['coding'][0]['code']
             else:
                 gender = ""
-            photo = getPicture(images,gender,bdate)
+            photo = getPicture(images,gender)
             if photo:
                 entries[i]['content']['photo'] = {}
                 entries[i]['content']['photo']['data'] = photo
