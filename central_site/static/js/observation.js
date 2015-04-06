f4b7d489e8157c44e4ed8af0d882fc3563e81369
wterrill <william.terrill@gmail.com>
2015-04-05 22:34:56 -0500

    graph systolic and diastolic blood pressure

--- central_site/static/js/observation.js
+++ central_site/static/js/observation.js
@@ -75,43 +75,86 @@ function plotChart(){
     var obs = $('.patient_card[patient_id="' + patientID + '"]').data("ObservationData");
     
     //Create our 'Store' to hold all our organized data;
-    var observationStore = {
+    var graphableObsStore = {
         'interpretation' : [],
         'types_of_observations' : []
     };
-    
+    var sysData = [];
+    var diaData = [];
+    var singleSysData = [];
+    var title1;
+    var title2;
+    var units;
     $.each(obs, function(o, observation) {
         //if observation has an interpretation, push it in the interpretation member
-        if(typeof observation.content.interpretation !== 'undefined') {
-            observationStore['interpretation'].push(observation);
-        } else {
+        if(typeof observation.content.interpretation !== 'undefined')  {
+            graphableObsStore['interpretation'].push(observation);
+        } 
+        else if (observation.content.name.coding[0].display === "Systolic Blood Pressure")
+        {
             //Grab the obersvation display name (lower casing to prevent case sensitive errors)
             var display = observation.content.name.coding[0].display.toLowerCase();
             //If this display is not in our array of types, add it and create a new empty array member in the 'Store'
-            if(observationStore['types_of_observations'].indexOf(display) === -1){
-                  observationStore['types_of_observations'].push(display);
-                  observationStore[display] = [];
+            if(graphableObsStore['types_of_observations'].indexOf(display) === -1){
+                  graphableObsStore['types_of_observations'].push(display);
+                  graphableObsStore[display] = [];
             }
+            singleSysData.push(observation.content.issued.valueOf());
+            singleSysData.push(observation.content.valueQuantity.value);
             //Push the observation in the appropriate member array
-            observationStore[display].push(observation);
+            sysData.push(singleSysData);
+            singleSysData = [];
+            graphableObsStore[display].push(observation);
+            title1 = observation.content.name.coding[0].display;
+            units = observation.content.valueQuantity.units;
+            
         }
+        else if (observation.content.name.coding[0].display === "Diastolic Blood Pressure")
+        {
+            //Grab the obersvation display name (lower casing to prevent case sensitive errors)
+            var display = observation.content.name.coding[0].display.toLowerCase();
+            //If this display is not in our array of types, add it and create a new empty array member in the 'Store'
+            if(graphableObsStore['types_of_observations'].indexOf(display) === -1){
+                  graphableObsStore['types_of_observations'].push(display);
+                  graphableObsStore[display] = [];
+            }
+            //var d = new Date();
+            //d = observation.content.issued;
+            singleSysData.push(observation.content.issued.valueOf());
+            singleSysData.push(observation.content.valueQuantity.value);
+            //Push the observation in the appropriate member array
+            diaData.push(singleSysData);
+            singleSysData = [];
+            graphableObsStore[display].push(observation);
+            title2 = observation.content.name.coding[0].display;
+            units = observation.content.valueQuantity.units;
+            
+        }
+       
     });
+    //sysData = [[Date.UTC(2010,1,4),100],[Date.UTC(2010,1,5),110]];
+    //sysData = [[Date.UTC(2010,1,4),123],[Date.UTC(2010,1,5),115]];
+    graphit(title1,title2,units,sysData,diaData);
     
+}
+    
+function graphit(title1,title2,units,sysData,diaData)
+{
     $('#myChart').highcharts({
                 chart: {
                     type: 'spline'
                 },
                 title: {
-                    text: 'Snow depth at Vikjafjellet, Norway'
+                    text: title1 + " and " + title2 
                 },
-                subtitle: {
-                    text: 'Irregular time data in Highcharts JS'
-                },
+                //subtitle: {
+                //    text: 'Irregular time data in Highcharts JS'
+                //},
                 xAxis: {
                     type: 'datetime',
                     dateTimeLabelFormats: { // don't display the dummy year
                         month: '%e. %b',
-                        year: '%b'
+                        year: '%Y'
                     },
                     title: {
                         text: 'Date'
@@ -119,7 +162,7 @@ function plotChart(){
                 },
                 yAxis: {
                     title: {
-                        text: 'Snow depth (m)'
+                        text: units
                     },
                     min: 0
                 },
@@ -135,84 +178,14 @@ function plotChart(){
                     }
                 },
                 series: [{
-                    name: 'Winter 2007-2008',
+                    name: title1,
                     // Define the data points. All series have a dummy year
                     // of 1970/71 in order to be compared on the same x axis. Note
                     // that in JavaScript, months start at 0 for January, 1 for February etc.
-                    data: [
-                        [Date.UTC(1970,  9, 27), 0   ],
-                        [Date.UTC(1970, 10, 10), 0.6 ],
-                        [Date.UTC(1970, 10, 18), 0.7 ],
-                        [Date.UTC(1970, 11,  2), 0.8 ],
-                        [Date.UTC(1970, 11,  9), 0.6 ],
-                        [Date.UTC(1970, 11, 16), 0.6 ],
-                        [Date.UTC(1970, 11, 28), 0.67],
-                        [Date.UTC(1971,  0,  1), 0.81],
-                        [Date.UTC(1971,  0,  8), 0.78],
-                        [Date.UTC(1971,  0, 12), 0.98],
-                        [Date.UTC(1971,  0, 27), 1.84],
-                        [Date.UTC(1971,  1, 10), 1.80],
-                        [Date.UTC(1971,  1, 18), 1.80],
-                        [Date.UTC(1971,  1, 24), 1.92],
-                        [Date.UTC(1971,  2,  4), 2.49],
-                        [Date.UTC(1971,  2, 11), 2.79],
-                        [Date.UTC(1971,  2, 15), 2.73],
-                        [Date.UTC(1971,  2, 25), 2.61],
-                        [Date.UTC(1971,  3,  2), 2.76],
-                        [Date.UTC(1971,  3,  6), 2.82],
-                        [Date.UTC(1971,  3, 13), 2.8 ],
-                        [Date.UTC(1971,  4,  3), 2.1 ],
-                        [Date.UTC(1971,  4, 26), 1.1 ],
-                        [Date.UTC(1971,  5,  9), 0.25],
-                        [Date.UTC(1971,  5, 12), 0   ]
-                    ]
+                    data: sysData
                 }, {
-                    name: 'Winter 2008-2009',
-                    data: [
-                        [Date.UTC(1970,  9, 18), 0   ],
-                        [Date.UTC(1970,  9, 26), 0.2 ],
-                        [Date.UTC(1970, 11,  1), 0.47],
-                        [Date.UTC(1970, 11, 11), 0.55],
-                        [Date.UTC(1970, 11, 25), 1.38],
-                        [Date.UTC(1971,  0,  8), 1.38],
-                        [Date.UTC(1971,  0, 15), 1.38],
-                        [Date.UTC(1971,  1,  1), 1.38],
-                        [Date.UTC(1971,  1,  8), 1.48],
-                        [Date.UTC(1971,  1, 21), 1.5 ],
-                        [Date.UTC(1971,  2, 12), 1.89],
-                        [Date.UTC(1971,  2, 25), 2.0 ],
-                        [Date.UTC(1971,  3,  4), 1.94],
-                        [Date.UTC(1971,  3,  9), 1.91],
-                        [Date.UTC(1971,  3, 13), 1.75],
-                        [Date.UTC(1971,  3, 19), 1.6 ],
-                        [Date.UTC(1971,  4, 25), 0.6 ],
-                        [Date.UTC(1971,  4, 31), 0.35],
-                        [Date.UTC(1971,  5,  7), 0   ]
-                    ]
-                }, {
-                    name: 'Winter 2009-2010',
-                    data: [
-                        [Date.UTC(1970,  9,  9), 0   ],
-                        [Date.UTC(1970,  9, 14), 0.15],
-                        [Date.UTC(1970, 10, 28), 0.35],
-                        [Date.UTC(1970, 11, 12), 0.46],
-                        [Date.UTC(1971,  0,  1), 0.59],
-                        [Date.UTC(1971,  0, 24), 0.58],
-                        [Date.UTC(1971,  1,  1), 0.62],
-                        [Date.UTC(1971,  1,  7), 0.65],
-                        [Date.UTC(1971,  1, 23), 0.77],
-                        [Date.UTC(1971,  2,  8), 0.77],
-                        [Date.UTC(1971,  2, 14), 0.79],
-                        [Date.UTC(1971,  2, 24), 0.86],
-                        [Date.UTC(1971,  3,  4), 0.8 ],
-                        [Date.UTC(1971,  3, 18), 0.94],
-                        [Date.UTC(1971,  3, 24), 0.9 ],
-                        [Date.UTC(1971,  4, 16), 0.39],
-                        [Date.UTC(1971,  4, 21), 0   ]
-                    ]
+                    name: title2,
+                    data: diaData
                 }]
             });
-    
-    
-    
 }
