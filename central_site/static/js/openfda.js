2dc6c6c09136530e1f235c2479c89cc318334901
Cheryl Miller <cmiller112000@sbcglobal.net>
2015-04-10 18:57:14 -0500

    Make initial accoridion states on openfda data as closed

--- central_site/static/js/openfda.js
+++ central_site/static/js/openfda.js
@@ -72,11 +72,11 @@ function MedicationOpenFDAtoHTML(data,rxNorm){
 	outhtml += "<div class='col-sm-4'>Generic Name: " + data.results[0].openfda.generic_name  + "</div>"; 
 	outhtml += "<div class='col-sm-4'>Manufacturer: " + data.results[0].openfda.manufacturer_name  + "</div>";
 	
-	outhtml += "<br><div class='accordion' id='openfda2'>";
+	outhtml += "<br/><br/><div class='accordion' id='openfda2'>";
 	outhtml += "<div class='accordion-group'>";
 	outhtml += "<div class='accordion-heading'>";
 	outhtml += "<a class='accordion-toggle' data-toggle='collapse' data-parent='#openfda2' href='#collapseOne'>Description</a>";
-	outhtml += "</div><div id='collapseOne' class='accordion-body collapse in'>";
+	outhtml += "</div><div id='collapseOne' class='accordion-body collapse'>";
 	outhtml += "<div class='accordion-inner'><br>";
 	outhtml +=  data.results[0].description === undefined ? 'No Description Information' : data.results[0].description;
 	outhtml += "<br><br></div></div></div>";
@@ -84,7 +84,7 @@ function MedicationOpenFDAtoHTML(data,rxNorm){
 	outhtml += "<div class='accordion-group'>";
 	outhtml += "<div class='accordion-heading'>";
 	outhtml += "<a class='accordion-toggle' data-toggle='collapse' data-parent='#openfda2' href='#collapseTwo'>Dosage/Adminstration Table</a>";
-	outhtml += "</div><div id='collapseTwo' class='accordion-body collapse in'>";
+	outhtml += "</div><div id='collapseTwo' class='accordion-body collapse'>";
 	outhtml += "<div class='accordion-inner'><br>";
 	outhtml +=  data.results[0].dosage_and_administration_table === undefined ? 'No Dosage/Administration Table Information' : data.results[0].dosage_and_administration_table;
 	outhtml += "<br><br></div></div></div>";
@@ -92,7 +92,7 @@ function MedicationOpenFDAtoHTML(data,rxNorm){
 	outhtml += "<div class='accordion-group'>";
 	outhtml += "<div class='accordion-heading'>";
 	outhtml += "<a class='accordion-toggle' data-toggle='collapse' data-parent='#openfda2' href='#collapseThree'>Warnings</a>";
-	outhtml += "</div><div id='collapseThree' class='accordion-body collapse in'>";
+	outhtml += "</div><div id='collapseThree' class='accordion-body collapse'>";
 	outhtml += "<div class='accordion-inner'><br>";
 	outhtml +=  data.results[0].warnings === undefined ? 'No Warning Information' : data.results[0].warnings;
 	outhtml += "<br><br></div></div></div>";
@@ -100,7 +100,7 @@ function MedicationOpenFDAtoHTML(data,rxNorm){
 	outhtml += "<div class='accordion-group'>";
 	outhtml += "<div class='accordion-heading'>";
 	outhtml += "<a class='accordion-toggle' data-toggle='collapse' data-parent='#openfda2' href='#collapseFour'>General Precautions</a>";
-	outhtml += "</div><div id='collapseFour' class='accordion-body collapse in'>";
+	outhtml += "</div><div id='collapseFour' class='accordion-body collapse'>";
 	outhtml += "<div class='accordion-inner'><br>";
 	outhtml +=  data.results[0].general_precautions === undefined ? 'No General Precaution Information' : data.results[0].general_precautions;
 	outhtml += "<br><br></div></div></div>";
@@ -108,7 +108,7 @@ function MedicationOpenFDAtoHTML(data,rxNorm){
 	outhtml += "<div class='accordion-group'>";
 	outhtml += "<div class='accordion-heading'>";
 	outhtml += "<a class='accordion-toggle' data-toggle='collapse' data-parent='#openfda2' href='#collapseFive'>Drug Interactions</a>";
-	outhtml += "</div><div id='collapseFive' class='accordion-body collapse in'>";
+	outhtml += "</div><div id='collapseFive' class='accordion-body collapse'>";
 	outhtml += "<div class='accordion-inner'><br>";
 	outhtml +=  data.results[0].drug_interactions === undefined ? 'No Drug Interaction Information' : data.results[0].drug_interactions;
 	outhtml += "<br><br></div></div></div>";
@@ -116,7 +116,7 @@ function MedicationOpenFDAtoHTML(data,rxNorm){
 	outhtml += "<div class='accordion-group'>";
 	outhtml += "<div class='accordion-heading'>";
 	outhtml += "<a class='accordion-toggle' data-toggle='collapse' data-parent='#openfda2' href='#collapseSix'>Adverse Reactions</a>";
-	outhtml += "</div><div id='collapseSix' class='accordion-body collapse in'>";
+	outhtml += "</div><div id='collapseSix' class='accordion-body collapse'>";
 	outhtml += "<div class='accordion-inner'><br>";
 	outhtml +=  data.results[0].adverse_reactions === undefined ? 'No Adverse Reaction Information' : data.results[0].adverse_reactions;
 	outhtml += "<br><br></div></div></div>";
